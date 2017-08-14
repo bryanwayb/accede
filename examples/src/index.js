5d332c921107a2a39254e97a520462bcf381848c
Bryan Way <bryanwayb@gmail.com>
2017-08-14 05:05:10 -0500

    Moved thread setup function to its own file

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,23 +1,139 @@
 const accede = require('../../index.js');
 
-let pool = new accede.threading.Pool(4, 8);
+let thread = new accede.threading.Thread(() => {
+    // Types
+    /// 0 = added
+    /// 1 = deleted
+    /// 2 = changed
+    thread.findDiffs = function findDiffs(left, right, diff, path) {
+        if(left != null) {
+            if(right == null) {
+                diff.push({
+                    t: 1,
+                    p: path
+                });
+            }
+            else if(typeof right !== typeof left) {
+                diff.push({
+                    t: 2,
+                    p: path
+                });
+            }
+            else if(typeof left === 'object') {
+                if(Array.isArray(left)) {
+                    let length = Math.max(left.length, right.length);
+                    for(let i = 0; i < length; i++) {
+                        findDiffs(left[i], right[i], diff, [...path.slice(0), i]);
+                    }
+                }
+                else {
+                    let leftKeys = Object.keys(left),
+                        rightKeys = Object.keys(right),
+                        found = {},
+                        keys = [...leftKeys, ...rightKeys].filter((value) => {
+                            return found[value] ? false : (found[value] = true);
+                        });
+                    
+                    for(let i = 0; i < keys.length; i++) {
+                        let key = keys[i];
+                        findDiffs(left[key], right[key], diff, [...path.slice(0), key]);
+                    }
+                }
+            }
+            else if(left != right) {
+                diff.push({
+                    t: 2,
+                    p: path
+                });
+            }
+        }
+        else if(right != null) {
+            diff.push({
+                t: 0,
+                p: path
+            });
+        }
+    };
+});
 
 (async () => {
-    await pool.start();
+    await thread.start();
+
+    let treeA = [
+        { // Node object
+            t: false, // Is text node? false == element node
+
+            // Below are only valid for element nodes
+            e: 'p', // Element name
+            a: [ // Element attributes
+                { // Attribute object
+                    n: 'class',
+                    v: 'testing'
+                }
+            ],
+            c: [ // Child nodes
+                { // Text node
+                    t: true, // Text node == true
+                    v: 'P tag testing' // Text value
+                },
+                { // Child element
+                    t: false,
+                    e: 'small',
+                    c: [
+                        {
+                            t: true,
+                            v: 'small text here'
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
+
+    let treeB = [
+        {
+            t: true,
+            v: 'stuff'
+        },
+        {
+            t: false,
+            e: 'p',
+            a: [
+                {
+                    n: 'class',
+                    v: 'testing'
+                }
+            ],
+            c: [
+                {
+                    t: true,
+                    v: 'P tag testing'
+                },
+                {
+                    t: false,
+                    e: 'small',
+                    c: [
+                        {
+                            t: true,
+                            v: 'small text here'
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
 
     let start = Date.now();
 
     console.log('started');
 
-    await pool.cluster(32, async () => {
-        return await new Promise((resolve) => {
-            setTimeout(() => {
-                resolve();
-            }, 50);
-        });
-    });
+    console.log(await thread.runInContext(async (left, right) => {
+        let diff = [];
+        this.findDiffs(left, right, diff, []);
+        return diff;
+    }, treeA, treeB));
 
     console.log(`Done in ${Date.now() - start}ms`)
 
-    await pool.kill();
+    await thread.kill();
 })();
