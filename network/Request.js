8d7515527f5ad521f8579866ecdc932b5ab32a06
Bryan Way <bryanwayb@gmail.com>
2017-09-09 19:32:54 -0500

    Working on adding abort handling for requests

--- network/Request.js
+++ network/Request.js
@@ -6,14 +6,25 @@ const Response = require('./Response'),
 let fetch = null;
 
 if(!process.env.ACCEDE_DISABLE_FETCH) {
-    fetch = window.fetch;
+    fetch = null;
 }
+
+const RequestCacheEnum = {
+    Default: 'default',
+    NoStore: 'no-store',
+    Reload: 'reload',
+    NoCache: 'no-cache',
+    ForceCache: 'force-cache',
+    OnlyIfCached: 'only-if-cached'
+};
 
 class Request {
     constructor(url, method = 'get', headers = null, body = null) {
         this.url = url;
         this.method = method;
         this.body = body;
+        this.cache = RequestCacheEnum.Default;
+        this.aborted = false;
 
         if(headers instanceof Headers) {
             this.headers = headers;
@@ -29,37 +40,75 @@ class Request {
     fetch() {
         return new Promise(async (resolve) => {
             if(fetch) {
+                let signal = null;
+                if(window.FetchController) {
+                    this._controller = new FetchController();
+                    signal = this._controller.signal;
+                }
+
                 resolve(fetch(this.url, {
                     method: this.method,
                     headers: this.headers.entries(),
-                    body: await this.body
+                    body: await this.body,
+                    cache: this.cache,
+                    signal
                 }).then((fetchResponse) => {
-                    return new Response(this, fetchResponse);
+                    let ret = null;
+
+                    if(!this._controller.signal.aborted) {
+                        ret = new Response(this, fetchResponse);
+                    }
+
+                    return ret;
                 }));
             }
             else {
-                let xhr = new XMLHttpRequest();
+                this._xhr = new XMLHttpRequest();
 
-                xhr.addEventListener('load', () => {
-                    if(xhr.readyState === 4) {
-                        resolve(new Response(this, xhr));
+                this._xhr.addEventListener('load', () => {
+                    if(this._xhr.readyState === 4) {
+                        if(!this.aborted) {
+                            resolve(new Response(this, this._xhr));
+                        }
+                        else {
+                            resolve(null);
+                        }
                     }
                 });
-                xhr.open(this.method, this.url);
+                this._xhr.open(this.method, this.url);
+
+                if(this.cache !== RequestCacheEnum.Default
+                    && this.cache !== RequestCacheEnum.ForceCache) {
+                    let cacheHeader = this.cache;
+
+                    if(cacheHeader === RequestCacheEnum.Reload) {
+                        cacheHeader = RequestCacheEnum.NoCache;
+                    }
+
+                    this.headers.set('cache-control', cacheHeader);
+                }
 
                 let headerKeys = this.headers.keys();
                 for(let i = 0; i < headerKeys.length; i++) {
-                    xhr.setRequestHeader(headerKeys[i], this.headers.get(headerKeys[i]));
+                    this._xhr.setRequestHeader(headerKeys[i], this.headers.get(headerKeys[i]));
                 }
 
-                xhr.send(await this.body);
+                this._xhr.send(await this.body);
             }
         });
     }
 
     abort() {
-
+        if(this._controller) {
+            this._controller.abort();
+        }
+        else if(this._xhr) {
+            this._xhr.abort();
+        }
+        this.aborted = true;
     }
 }
+
+Request.Cache = RequestCacheEnum;
 
 module.exports = Request;
